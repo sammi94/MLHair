5edc31df307881f2f481d394b5867fb2dce25c33
Masqurin <Masqurin@icloud.com>
2017-09-06 21:15:06 +0800

    table修復

---
+++ MLHair/AvatorTViCell.h
@@ -0,0 +1,17 @@
+//
+//  AvatorTViCell.h
+//  MLHair
+//
+//  Created by Ｍasqurin on 2017/9/6.
+//  Copyright © 2017年 sammi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AvatorTViCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *avator;
+@property (weak, nonatomic) IBOutlet UILabel *name;
+
+
+@end
