b7cf5682aba584c2dd18921b1fa58b5face42559
Masqurin <Masqurin@icloud.com>
2017-09-15 07:55:55 +0800

    準備合併

---
+++ MLHair/BookingDataVC.h
@@ -0,0 +1,16 @@
+//
+//  BookingDataVC.h
+//  MLHair
+//
+//  Created by Ｍasqurin on 2017/9/15.
+//  Copyright © 2017年 sammi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BookingVO.h"
+
+@interface BookingDataVC : UIViewController
+
+@property (nonatomic,strong) NSArray <BookingVO*>*bookingList;
+
+@end
